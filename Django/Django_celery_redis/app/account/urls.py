2c27f10fbf5af615d7beda321221a3b72ec7146c
sedm1n <4310481@gmail.com>
2024-10-09 13:11:01 +0300

    add password reset: urls, templates

--- Django/Django_celery_redis/app/account/urls.py
+++ Django/Django_celery_redis/app/account/urls.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
-from django.urls import include, path
+from django.urls import include, path, reverse_lazy
 from .views import dashboard_view, delete_user_view, profile_manegment_view, register_view , user_login_view, user_logout_view
 
 app_name = "account"
@@ -11,7 +12,7 @@ urlpatterns = [
            lambda request: render(request, 
                                   "account/email/verification_sent.html") ,
                                     name="email_verification_sent"),
-      path('password_reset/', include('django.contrib.auth.urls'), name="password_reset"),
+      
       
       path("login/", user_login_view, name="login"),
       path("logout/", user_logout_view, name="logout"),
@@ -19,4 +20,28 @@ urlpatterns = [
       path('dashboard/', dashboard_view, name="dashboard"),
       path('profile-managment/', profile_manegment_view, name="profile-management"),
       path('delete-user', delete_user_view, name="delete-user"),
+
+      # password reset
+      path('password-reset/',
+           auth_views.PasswordResetView.as_view(
+               success_url=reverse_lazy('account:password_reset_done'),
+               template_name='account/password/password_reset_form.html',
+               email_template_name='account/password/password_reset_email.html'
+           ), name='password-reset'),
+
+      path('password-reset/done/',
+           auth_views.PasswordResetDoneView.as_view(
+               template_name='account/password/password_reset_done.html'
+           ), name='password-reset-done'),
+
+      path('password-reset-confirm/<uidb64>/<token>/',
+           auth_views.PasswordResetConfirmView.as_view(
+               success_url=reverse_lazy('account:password_reset_complete'),
+               template_name='account/password/password_reset_confirm.html'
+           ), name='password-reset-confirm'),
+
+      path('password-reset-complete/',
+           auth_views.PasswordResetCompleteView.as_view(
+               template_name='account/password/password_reset_complete.html'
+           ), name='password-reset-complete'),
 ]
