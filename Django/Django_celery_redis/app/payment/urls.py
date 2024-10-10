cbf5abbae534b4679250749de47de41b3ca8bcca
sedm1n <4310481@gmail.com>
2024-10-10 12:50:51 +0300

    add urls webhook, add stripe webhook in webhooks

--- Django/Django_celery_redis/app/payment/urls.py
+++ Django/Django_celery_redis/app/payment/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+
 from . import views
+from .webhooks import stripe_webhook
 
 app_name = "payment"
 
@@ -10,6 +12,7 @@ urlpatterns = [
       path("payment_success/", views.payment_success_view, name="payment_success"),
       path("payment_fail/", views.payment_fail_view, name="payment_fail"),
       path("shipping/", views.shipping_view, name="shipping"),
+      path("webhook/", stripe_webhook, name="webhook"),
       
 
 ]
---
+++ Django/Django_celery_redis/app/payment/webhooks.py
@@ -0,0 +1,45 @@
+import stripe
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from yookassa import Configuration, Payment, Webhook
+from yookassa.domain.common import SecurityHelper
+from yookassa.domain.notification import (WebhookNotificationEventType,
+                                          WebhookNotificationFactory)
+
+from .models import Order
+from .tasks import send_order_confirmation
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+    webhook_secret = settings.STRIPE_WEBHOOK_SECRET
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, webhook_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the event
+    if event["type"] == "checkout.session.completed":
+        session = event["data"]["object"]
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                  order_id = session.client_reference_id
+            except Order.DoesNotExist:
+                return HttpResponse(status=404)
+            
+        order = Order.objects.get(id=order_id)
+        order.paid = True
+        order.save()
+
+    return HttpResponse(status=200)
