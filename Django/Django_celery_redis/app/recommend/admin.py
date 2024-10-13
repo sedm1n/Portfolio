ba53f76c157ed2fa54bdfcd817a0955c5270e7c6
sedm1n <4310481@gmail.com>
2024-10-13 13:53:01 +0300

    add ReviewAdmin , register admins

--- Django/Django_celery_redis/app/recommend/admin.py
+++ Django/Django_celery_redis/app/recommend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+class ReviewAdmin(admin.ModelAdmin):
+      list_display = ['product', 'created_by', 'rating', 'created_at']
+
+admin.site.register(Review, ReviewAdmin)
