ad04efdfb0a06e2a033a5dac0f607a71ed7680e3
sedm1n <4310481@gmail.com>
2024-10-13 10:52:21 +0300

    change Order and OrderItems models: add methods and properties

--- Django/Django_celery_redis/app/payment/models.py
+++ Django/Django_celery_redis/app/payment/models.py
@@ -1,7 +1,10 @@
+from decimal import Decimal
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 from shop.models import Product
 
@@ -18,8 +21,31 @@ class ShippingAdress(models.Model):
     zip = models.CharField(max_length=12, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = "Shipping Adress"
+        verbose_name_plural = "Shipping Adresses"
+        ordering = ['-id']
+
     def __str__(self):
-        return "ShippingAdress object: " + str(self.id)
+        return "ShippingAdress object: " + " - " + self.full_name
+    
+    def get_absolute_url(self):
+        return f"{reverse('payment:shipping')}"
+    
+    @classmethod
+    def create_default_shipping_address(cls, user):
+        default_shipping_address = {'user': user,
+                                    'full_name': "Noname",
+                                    'email': "example@example.com",
+                                    'street_adress': "street",
+                                    'apartment_adress': "apartment",
+                                    'city': "city",
+                                    'country': "country",
+                                    'zip': "12345"}
+        shipping_address = cls(**default_shipping_address)
+        shipping_address.save()
+        return shipping_address
+    
 
 
 class Order(models.Model):
@@ -31,9 +57,33 @@ class Order(models.Model):
     )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid = models.BooleanField(default=False)
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0),MaxValueValidator(100)])
+
+    class Meta:
+        verbose_name = "Order"
+        verbose_name_plural = "Orders"
+        ordering = ['-created']
+        indexes=[models.Index(fields=['-created'])]
+
+        constraints= [
+            models.CheckConstraint(check=models.Q(amount__gte=0), name="amount_gte_0"),   
+        ]
 
     def __str__(self):
         return "Order object: " + str(self.id)
+    
+    def get_absolute_url(self):
+        return reverse("payment:order_detail", kwargs={"pk": self.pk})
+
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    @property
+    def get_discount(self):
+        if (total_cost := self.get_total_cost_before_discount()) and self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
+    
 
 
 class OrderItem(models.Model):
@@ -43,5 +93,27 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
+
+    class Meta:
+        verbose_name = "OrderItem"
+        verbose_name_plural = "OrderItems"
+        ordering = ['-id']
+
+        constraints= [
+            models.CheckConstraint(check=models.Q(quantity__gte=0), name="quantity_gte_0"),   
+        ]
+
     def __str__(self):
         return "OrderItem object: " + str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
+    @classmethod
+    def get_total_quantity_for_product(cls, product):
+        return cls.objects.filter(product=product).aggregate(
+            total_quantity=models.Sum('quantity'))['total_quantity'] or 0
+    
+    @staticmethod
+    def average_price():
+        return OrderItem.objects.aggregate(models.Avg('price'))['average_price']
