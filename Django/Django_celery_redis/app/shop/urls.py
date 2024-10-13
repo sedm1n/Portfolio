40c1329b44428804c1eb90d8f437e54e99428adb
sedm1n <4310481@gmail.com>
2024-10-13 13:27:38 +0300

    change Product model: fix path to default images path

--- Django/Django_celery_redis/app/shop/urls.py
+++ Django/Django_celery_redis/app/shop/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import products_view, product_detail_view, category_list_view
+from .views import ProductListView, product_detail_view, category_list_view
 
 app_name = "shop"
 
 urlpatterns = [
-      path("", products_view, name="products"),
+      path("", ProductListView.as_view(), name="products"),
       path("<slug:slug>/", product_detail_view, name="product_detail"),
       path("search/<slug:slug>/", category_list_view, name="category_list"),
 ]
--- Django/Django_celery_redis/app/shop/views.py
+++ Django/Django_celery_redis/app/shop/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from .models import Category, ProductProxy
 
 
-def products_view(request):
-      products = ProductProxy.objects.all()
 
-      return render(request, "shop/products.html", {"products": products})
+class ProductListView(ListView):
+      model = ProductProxy
+      template_name = "shop/products.html"
+      context_object_name = "products"
+
+      paginate_by = 15
+
+      def get_template_names(self) -> list[str]:
+            if self.request.htmx:
+                  return ["shop/components/product_list.html"]
+            return "shop/products.html"
+
+      def get_queryset(self):
+            return ProductProxy.objects.all()
+
+# def products_view(request):
+#       products = ProductProxy.objects.all()
+
+#       return render(request, "shop/products.html", {"products": products})
 
 def product_detail_view(request, slug):
       product = get_object_or_404(ProductProxy, slug=slug)
